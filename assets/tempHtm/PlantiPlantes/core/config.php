7d84a951b1ae1a10b3ca39f5ef0bbad50d815658
Tentation-G <f.ca3@aol.com>
2025-05-14 16:23:31 +0200

    maj, liens client par github

--- assets/tempHtm/PlantiPlantes/core/config.php
+++
@@ -1,7 +0,0 @@
-<?php
-// define('URL_ASSETS','http://localhost/projets/plantes-mvc/assets/'); 
-// define('URL','http://localhost/projets/plantes-mvc/');
-
-define('URL_ASSETS','http://localhost/PlantiPlantes/assets/'); 
-define('URL','http://localhost/PlantiPlantes/');
-session_start();
